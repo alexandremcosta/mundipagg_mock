f0928d4621816857751921b42e505409e556476e
Bernardo Amorim <contato@bamorim.com>
2014-07-29 13:17:46 -0300

    Responders fixed and callables

--- lib/mundipagg_mock.rb
+++ lib/mundipagg_mock.rb
@@ -1,13 +1,21 @@
 module MundipaggMock
   class << self
     attr_reader :calls
+    attr_accessor :responder
+
     def setup
       @calls = []
+      @responder = ->(hash,method){nil}
       Mundipagg::Gateway.instance_eval do
         define_method :SendToService do |hash, method|
           MundipaggMock.calls << {hash: hash, method_name: method}
+          MundipaggMock.responder.call(hash, method)
         end
       end
+    end
+
+    def respond_with response
+      self.responder = ->(hash,method){ response }
     end
   end
 end
--- test/unit/mundipagg_mock_test.rb
+++ test/unit/mundipagg_mock_test.rb
@@ -13,6 +13,18 @@ describe MundipaggMock do
     MundipaggMock.calls.last[:method_name].must_equal :method
   end
 
-  it 'should enable setting up the responses' do
+  describe '.respond_with' do
+    it 'should enable setting up the responses' do
+      MundipaggMock.respond_with key: "value"
+      @client.SendToService({},:method).must_equal key: "value"
+    end
+  end
+
+  describe '.responder=' do
+    it 'should enable setting responders' do
+      MundipaggMock.responder = ->(hash, method){ method }
+      @client.SendToService({},:a).must_equal :a
+      @client.SendToService({},:b).must_equal :b
+    end
   end
 end
